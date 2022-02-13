68b6a5c827df19d6cb8a3e7870134a5337f0c3fa
Kevin Tindall <kevinkjt2000@gmail.com>
2022-02-13 10:34:16 -0600

    Theme the docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,9 @@ author = "Nathan Adams"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -43,7 +45,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
