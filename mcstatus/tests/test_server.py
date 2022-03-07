153a594de65293de53f9579e968f129c9ec235dd
ItsDrike <itsdrike@protonmail.com>
2022-03-07 13:29:07 +0100

    Remove redundant tests in server

--- mcstatus/tests/test_server.py
+++ mcstatus/tests/test_server.py
@@ -2,9 +2,7 @@ import asyncio
 import sys
 from unittest.mock import Mock, patch
 
-import dns.resolver
 import pytest
-from dns.rdatatype import RdataType
 
 from mcstatus.protocol.connection import Connection
 from mcstatus.server import MinecraftServer
@@ -175,35 +173,3 @@ class TestMinecraftServer:
                 with pytest.raises(Exception):
                     self.server.query()
                 assert querier.call_count == 3
-
-    def test_by_address_no_srv(self):
-        with patch("dns.resolver.resolve") as resolve:
-            resolve.side_effect = [dns.resolver.NXDOMAIN]
-            self.server = MinecraftServer.lookup("example.org", timeout=3)
-            resolve.assert_called_once_with("_minecraft._tcp.example.org", RdataType.SRV, lifetime=3)
-        assert self.server.host == "example.org"
-        assert self.server.port == 25565
-
-    def test_by_address_with_srv(self):
-        with patch("dns.resolver.resolve") as resolve:
-            answer = Mock()
-            answer.target = "different.example.org."
-            answer.port = 12345
-            resolve.return_value = [answer]
-            self.server = MinecraftServer.lookup("example.org", timeout=3)
-            resolve.assert_called_once_with("_minecraft._tcp.example.org", RdataType.SRV, lifetime=3)
-        assert self.server.host == "different.example.org"
-        assert self.server.port == 12345
-
-    def test_by_address_with_port(self):
-        self.server = MinecraftServer.lookup("example.org:12345")
-        assert self.server.host == "example.org"
-        assert self.server.port == 12345
-
-    def test_by_address_with_multiple_ports(self):
-        with pytest.raises(ValueError):
-            MinecraftServer.lookup("example.org:12345:6789")
-
-    def test_by_address_with_invalid_port(self):
-        with pytest.raises(ValueError):
-            MinecraftServer.lookup("example.org:port")
