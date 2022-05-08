126cf7a0e6b2f34c6a8eec4a70dcb37d8dd6daf5
PerchunPak <perchunpak@gmail.com>
2022-05-08 19:59:14 +0300

    Change `==` to `is` in `status_response.py`
    Co-authored-by: ItsDrike <itsdrike@protonmail.com>

--- mcstatus/status_response.py
+++ mcstatus/status_response.py
@@ -40,7 +40,7 @@ class AbstractDataclass(ABC):
     """
 
     def __new__(cls, *args, **kwargs):
-        if cls == AbstractDataclass or cls.__bases__[0] == AbstractDataclass:
+        if cls is AbstractDataclass or cls.__bases__[0] is AbstractDataclass:
             raise TypeError("Cannot instantiate abstract class.")
         return super().__new__(cls)
 
