cfcd811116aefbcd4ac5148cef0475221e09f50a
PerchunPak <perchunpak@gmail.com>
2022-05-08 12:50:00 +0200

    Add test for types in bedrock answer

--- mcstatus/tests/test_bedrock_status.py
+++ mcstatus/tests/test_bedrock_status.py
@@ -1,5 +1,5 @@
 from mcstatus.bedrock_status import BedrockServerStatus
-from mcstatus.status_response import BedrockServerResponse
+from mcstatus.status_response import BedrockServerPlayers, BedrockServerResponse, BedrockServerVersion
 
 
 def test_bedrock_response_contains_expected_fields():
@@ -10,7 +10,6 @@ def test_bedrock_response_contains_expected_fields():
         b"3022;;Default;1;19132;-1;"
     )
     parsed = BedrockServerStatus.parse_response(data, 1)
-    assert isinstance(parsed, BedrockServerResponse)
     assert "motd" in parsed.__dict__
     assert "latency" in parsed.__dict__
     assert "map_name" in parsed.__dict__
@@ -24,3 +23,27 @@ def test_bedrock_response_contains_expected_fields():
     assert "name" in parsed.version.__dict__
     assert "protocol" in parsed.version.__dict__
     assert "brand" in parsed.version.__dict__
+
+
+def test_bedrock_response_have_right_types():
+    data = (
+        b"\x1c\x00\x00\x00\x00\x00\x00\x00\x004GT\x00\xb8\x83D\xde\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd"
+        b"\x124Vx\x00wMCPE;\xc2\xa7r\xc2\xa74G\xc2\xa7r\xc2\xa76a\xc2\xa7r\xc2\xa7ey\xc2\xa7r\xc2\xa72B\xc2\xa7r\xc2"
+        b"\xa71o\xc2\xa7r\xc2\xa79w\xc2\xa7r\xc2\xa7ds\xc2\xa7r\xc2\xa74e\xc2\xa7r\xc2\xa76r;422;;1;69;376707197539105"
+        b"3022;;Default;1;19132;-1;"
+    )
+    parsed = BedrockServerStatus.parse_response(data, 1)
+    assert isinstance(parsed, BedrockServerResponse)
+    assert type(parsed.motd) is str
+    assert type(parsed.latency) is float
+    assert type(parsed.map_name) is str
+    assert type(parsed.gamemode) is str
+    assert type(parsed.players) is BedrockServerPlayers
+    assert type(parsed.version) is BedrockServerVersion
+
+    assert type(parsed.players.online) is int
+    assert type(parsed.players.max) is int
+
+    assert type(parsed.version.name) is str
+    assert type(parsed.version.protocol) is int
+    assert type(parsed.version.brand) is str
