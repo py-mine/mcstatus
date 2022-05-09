fcbe235141eb3131390338070dc674975e9e1cd2
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-05-09 17:40:09 -0500

    Oh no, two seperate were doing the same thing

--- mcstatus/protocol/connection.py
+++ mcstatus/protocol/connection.py
@@ -14,10 +14,7 @@ from ipaddress import ip_address
 from typing import Iterable, Optional, TYPE_CHECKING, Union
 
 import asyncio_dgram
-if TYPE_CHECKING:
-    from typing_extensions import SupportsIndex  # Python 3.7 doesn't support this yet.
-
-    BytesConvertable = Union[SupportsIndex, Iterable[SupportsIndex]]
+from typing_extensions import SupportsIndex
 
 if TYPE_CHECKING:
     BytesConvertable = Union[SupportsIndex, Iterable[SupportsIndex]]
