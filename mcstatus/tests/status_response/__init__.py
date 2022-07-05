44a4373df5d47193c26696f4a71a2a0e0f485b83
PerchunPak <perchunpak@gmail.com>
2022-07-05 19:11:49 +0200

    Fix flake8's error about types in `does_not_raise`

--- mcstatus/tests/status_response/__init__.py
+++ mcstatus/tests/status_response/__init__.py
@@ -1,9 +1,10 @@
 from contextlib import contextmanager
+from typing import Iterable
 
 __all__ = ["does_not_raise"]
 
 
-@contextmanager
-def does_not_raise():
+@contextmanager  # type: ignore # iterable type is valid for contextmanager, don't know what wrong with pyright
+def does_not_raise() -> Iterable[None]:
     """Uses in parameterized tests to ensure that the test does not raise an exception."""
     yield
