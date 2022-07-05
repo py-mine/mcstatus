2055c5a7daf2a68a1aac678f3593ec4ae61a236d
PerchunPak <perchunpak@gmail.com>
2022-07-05 11:46:07 +0300

    Add `__all__` to `__init__.py`

--- mcstatus/__init__.py
+++ mcstatus/__init__.py
@@ -1,1 +1,8 @@
 from mcstatus.server import BedrockServer, JavaServer, MinecraftBedrockServer, MinecraftServer  # noqa: F401
+
+__all__ = [
+    "BedrockServer",
+    "JavaServer",
+    "MinecraftBedrockServer",
+    "MinecraftServer",
+]
