8a4d9b4050ad49c34dad55319f3553594c34b419
PerchunPak <perchunpak@gmail.com>
2022-08-24 17:49:28 +0200

    Change `Iterable` type to `Iterator`
    As suggested @ItsDrike

--- tests/status_response/__init__.py
+++ tests/status_response/__init__.py
@@ -1,10 +1,10 @@
 from contextlib import contextmanager
-from typing import Iterable
+from typing import Iterator
 
 __all__ = ["does_not_raise"]
 
 
-@contextmanager  # type: ignore # iterable type is valid for contextmanager, don't know what wrong with pyright
-def does_not_raise() -> Iterable[None]:
+@contextmanager
+def does_not_raise() -> Iterator[None]:
     """Uses in parameterized tests to ensure that the test does not raise an exception."""
     yield
