2ac6fd043c4fd6ce46096ca2fc322a31713f7821
PerchunPak <git@perchun.it>
2023-05-24 19:38:32 +0200

    Ignore false positives

--- tests/status_response/test_shared.py
+++ tests/status_response/test_shared.py
@@ -6,4 +6,4 @@ from mcstatus.status_response import BaseStatusResponse
 class TestMCStatusResponse:
     def test_raises_not_implemented_error_on_build(self):
         with raises(NotImplementedError):
-            BaseStatusResponse.build({"foo": "bar"})
+            BaseStatusResponse.build({"foo": "bar"})  # type: ignore # method is abstract
