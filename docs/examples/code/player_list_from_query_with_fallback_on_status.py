cf6af6fd080bf01be9dca7adfcfddd3f0f531e00
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2023-12-16 11:51:44 -0600

    Use official pre-commit hooks and add codespell

--- docs/examples/code/player_list_from_query_with_fallback_on_status.py
+++ docs/examples/code/player_list_from_query_with_fallback_on_status.py
@@ -9,6 +9,6 @@ else:
     status = server.status()
 
     if not status.players.sample:
-        print("Cant find players list, no one online or the server disabled this.")
+        print("Can't find players list, no one online or the server disabled this.")
     else:
         print("Players online:", ", ".join([player.name for player in status.players.sample]))
