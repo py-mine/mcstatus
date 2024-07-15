60e3512494c9478e54eeb9ab749839c79e2920cf
rina <k@rina.fyi>
2024-07-15 22:51:23 +1000

    support Bedrock servers in CLI
    done in a slightly ad-hoc way, but this is the best
    we can do given the split of the response types.

--- mcstatus/__main__.py
+++ mcstatus/__main__.py
@@ -1,57 +1,76 @@
 from __future__ import annotations
 
+import sys
 import argparse
 import socket
+from typing import TYPE_CHECKING
 from json import dumps as json_dumps
 
-from mcstatus import JavaServer
+from mcstatus import JavaServer, BedrockServer
+from mcstatus.responses import JavaStatusResponse
+
+if TYPE_CHECKING:
+    SupportedServers = JavaServer | BedrockServer
+
+def ping(server: SupportedServers) -> int:
+    print(f"{server.ping():.2f}ms")
+    return 0
 
 
-def ping(server: JavaServer) -> None:
-    print(f"{server.ping()}ms")
-
-
-def status(server: JavaServer) -> None:
+def status(server: SupportedServers) -> int:
     response = server.status()
-    if response.players.sample is not None:
-        player_sample = str([f"{player.name} ({player.id})" for player in response.players.sample])
+
+    java_res = response if isinstance(response, JavaStatusResponse) else None
+
+    if java_res and java_res.players.sample is not None:
+        player_sample = [f"{player.name} ({player.id})" for player in java_res.players.sample]
     else:
         player_sample = "No players online"
 
     print(f"version: v{response.version.name} (protocol {response.version.protocol})")
-    print(f'motd: "{response.motd}"')
+    print(f'motd: {response.motd.to_ansi()}')
     print(f"players: {response.players.online}/{response.players.max} {player_sample}")
+    print(f"ping: {response.latency:.2f} ms")
+    return 0
 
 
-def json(server: JavaServer) -> None:
+def json(server: SupportedServers) -> int:
     data = {}
     data["online"] = False
     # Build data with responses and quit on exception
     try:
         status_res = server.status(tries=1)
+        java_res = status_res if isinstance(status_res, JavaStatusResponse) else None
         data["version"] = status_res.version.name
         data["protocol"] = status_res.version.protocol
         data["motd"] = status_res.motd.raw
         data["player_count"] = status_res.players.online
         data["player_max"] = status_res.players.max
         data["players"] = []
-        if status_res.players.sample is not None:
-            data["players"] = [{"name": player.name, "id": player.id} for player in status_res.players.sample]
+        if java_res and java_res.players.sample is not None:
+            data["players"] = [{"name": player.name, "id": player.id} for player in java_res.players.sample]
 
         data["ping"] = status_res.latency
         data["online"] = True
 
-        query_res = server.query(tries=1)  # type: ignore[call-arg] # tries is supported with retry decorator
-        data["host_ip"] = query_res.raw["hostip"]
-        data["host_port"] = query_res.raw["hostport"]
-        data["map"] = query_res.map
-        data["plugins"] = query_res.software.plugins
+        if isinstance(server, JavaServer):
+            query_res = server.query(tries=1)
+            data["host_ip"] = query_res.raw["hostip"]
+            data["host_port"] = query_res.raw["hostport"]
+            data["map"] = query_res.map
+            data["plugins"] = query_res.software.plugins
     except Exception:  # TODO: Check what this actually excepts
         pass
+
     print(json_dumps(data))
+    return 0
 
 
-def query(server: JavaServer) -> None:
+def query(server: SupportedServers) -> int:
+    if not isinstance(server, JavaServer):
+        print("The 'query' protocol is only supported by Java servers.", file=sys.stderr)
+        return 1
+
     try:
         response = server.query()
     except socket.timeout:
@@ -59,17 +78,20 @@ def query(server: JavaServer) -> None:
             "The server did not respond to the query protocol."
             "\nPlease ensure that the server has enable-query turned on,"
             " and that the necessary port (same as server-port unless query-port is set) is open in any firewall(s)."
-            "\nSee https://wiki.vg/Query for further information."
+            "\nSee https://wiki.vg/Query for further information.",
+            file=sys.stderr,
         )
-        return
+        return 1
+
     print(f"host: {response.raw['hostip']}:{response.raw['hostport']}")
     print(f"software: v{response.software.version} {response.software.brand}")
     print(f"plugins: {response.software.plugins}")
-    print(f'motd: "{response.motd}"')
+    print(f'motd: {response.motd.to_ansi()}')
     print(f"players: {response.players.online}/{response.players.max} {response.players.names}")
+    return 0
 
 
-def main() -> None:
+def main(argv: list[str]) -> int:
     parser = argparse.ArgumentParser(
         "mcstatus",
         description="""
@@ -80,6 +102,8 @@ def main() -> None:
     )
 
     parser.add_argument("address", help="The address of the server.")
+    parser.add_argument('--bedrock', help="Server is a Bedrock server (default: Java).",
+                        action='store_true')
 
     subparsers = parser.add_subparsers(title='commands', description="Command to run, defaults to 'status'.")
     parser.set_defaults(func=status)
@@ -96,11 +120,11 @@ def main() -> None:
         help="Prints server status and query in json. Supported by all Minecraft servers that are version 1.7 or higher.",
     ).set_defaults(func=json)
 
-    args = parser.parse_args()
-    server = JavaServer.lookup(args.address)
+    args = parser.parse_args(argv)
+    lookup = JavaServer.lookup if not args.bedrock else BedrockServer.lookup
+    server = lookup(args.address)
 
-    args.func(server)
-
+    return args.func(server)
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv[1:]))
