a7b065b55e34a31aec900d986a0796fd1bf5eba3
ItsDrike <itsdrike@protonmail.com>
2025-02-07 15:47:06 +0100

    Only install tomli for python<3.11

--- docs/conf.py
+++ docs/conf.py
@@ -16,10 +16,14 @@ from __future__ import annotations
 import os
 import sys
 from datetime import date
+from typing import TYPE_CHECKING
 
 from packaging.version import parse as parse_version
 
-if sys.version_info >= (3, 11):
+# Since pyright assumes lowest supported version, it would default to using tomli
+# which we don't want, as it's only installed if on <3.11 and pyright usually runs
+# on latest versions. For that reason, we include or TYPE_CHECKING in the condition.
+if sys.version_info >= (3, 11) or TYPE_CHECKING:
     from tomllib import load as toml_parse
 else:
     from tomli import load as toml_parse
