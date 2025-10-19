5f4d112b30f936a51187a62c7b9971f5679fbec5
Perchun Pak <git@perchun.it>
2025-10-20 00:55:50 +0200

    Consider falsely value as value when parsing Forge's `modid` (#1051)

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+import pytest
+
+from mcstatus.utils import or_none
+
+
+@pytest.mark.parametrize(
+    "a,b,result",
+    [
+        (None, None, None),
+        (None, "", ""),
+        ("", None, ""),
+        ("a", "b", "a"),
+    ],
+)
+def test_or_none(a, b, result):
+    assert or_none(a, b) == result
+
+
+def test_or_none_many_arguments():
+    assert or_none(*([None] * 100 + ["value"])) == "value"
