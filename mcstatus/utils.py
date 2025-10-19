d57f21c2c1711254574ffbc33b7bfba0e9f215ed
PerchunPak <git@perchun.it>
2025-10-19 20:59:53 +0200

    Rewrite `or_none` to accept arbitrary number of arguments

--- mcstatus/utils.py
+++ mcstatus/utils.py
@@ -16,7 +16,6 @@ else:
     P = []
 
 T = TypeVar("T")
-T2 = TypeVar("T2")
 R = TypeVar("R")
 R2 = TypeVar("R2")
 
@@ -186,19 +185,35 @@ def deprecated(
     return decorate
 
 
-def or_none(a: T, b: T2) -> T | T2 | None:
-    """Do inline ``or`` operation, but consider falsely values valid.
+def or_none(*args: T) -> T | None:
+    """Return the first non-None argument.
+
+    This function is similar to the standard inline ``or`` operator, while
+    treating falsey values (such as ``0``, ``''``, or ``False``) as valid
+    results rather than skipping them. It only skips ``None`` values.
+
+    This is useful when selecting between optional values that may be empty
+    but still meaningful.
 
     Example:
         .. code-block:: py
+            >>> or_none("", 0, "fallback")
+            ''
+            >>> or_none(None, None, "value")
+            'value'
+            >>> or_none(None, None)
+            None
+
+        This is often useful when working with dict.get, e.g.:
+
+         .. code-block:: py
             >>> mydict = {"a": ""}
             >>> mydict.get("a") or mydict.get("b")
-            None  # expected ""!
+            None  # expected ''!
             >>> or_none(mydict.get("a"), mydict.get("b"))
             ''
     """
-    if a is not None:
-        return a
-    if b is not None:
-        return b
+    for arg in args:
+        if arg is not None:
+            return arg
     return None
