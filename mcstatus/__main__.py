c6113c56bfdd1577fcf38ac91ac8f7e22ecf8033
Perchun Pak <git@perchun.it>
2026-01-07 17:57:30 +0100

    Drop Python 3.9 (#1060)

--- mcstatus/__main__.py
+++ mcstatus/__main__.py
@@ -6,16 +6,13 @@ import json
 import argparse
 import socket
 import dataclasses
-from typing import TYPE_CHECKING
+from typing import TypeAlias
 
 from mcstatus import JavaServer, BedrockServer
 from mcstatus.responses import JavaStatusResponse
 from mcstatus.motd import Motd
 
-if TYPE_CHECKING:
-    from typing_extensions import TypeAlias
-
-    SupportedServers: TypeAlias = "JavaServer | BedrockServer"
+SupportedServers: TypeAlias = "JavaServer | BedrockServer"
 
 PING_PACKET_FAIL_WARNING = (
     "warning: contacting {address} failed with a 'ping' packet but succeeded with a 'status' packet,\n"
