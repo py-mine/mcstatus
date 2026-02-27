b3726a1f970832e1b8fa70fccd260dbb9462f389
Perchun Pak <git@perchun.it>
2026-02-27 08:41:51 +0100

    Add tests for `LegacyClient.parse_response` (#1120)

--- tests/protocol/test_legacy_client.py
+++ tests/protocol/test_legacy_client.py
@@ -2,11 +2,41 @@ import pytest
 
 from mcstatus._protocol.connection import Connection
 from mcstatus._protocol.legacy_client import LegacyClient
+from mcstatus.motd import Motd
+from mcstatus.responses.legacy import LegacyStatusPlayers, LegacyStatusResponse, LegacyStatusVersion
 
 
 def test_invalid_kick_reason():
     with pytest.raises(IOError, match=r"^Received invalid kick packet reason$"):
         LegacyClient.parse_response("Invalid Reason".encode("UTF-16BE"), 123.0)
+
+
+@pytest.mark.parametrize(
+    ("response", "expected"),
+    [
+        (
+            "A Minecraft Server§0§20".encode("UTF-16BE"),
+            LegacyStatusResponse(
+                players=LegacyStatusPlayers(online=0, max=20),
+                version=LegacyStatusVersion(name="<1.4", protocol=-1),
+                motd=Motd.parse("A Minecraft Server"),
+                latency=123.0,
+            ),
+        ),
+        (
+            "§1\x0051\x001.4.7\x00A Minecraft Server\x000\x0020".encode("UTF-16BE"),
+            LegacyStatusResponse(
+                players=LegacyStatusPlayers(online=0, max=20),
+                version=LegacyStatusVersion(name="1.4.7", protocol=51),
+                motd=Motd.parse("A Minecraft Server"),
+                latency=123.0,
+            ),
+        ),
+    ],
+    ids=["b1.8", "1.4.7"],
+)
+def test_parse_response(response: bytes, expected: LegacyStatusResponse):
+    assert LegacyClient.parse_response(response, 123.0) == expected
 
 
 def test_invalid_packet_id():
